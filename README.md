d63a53bb2d7adb94cf5dea86c4e6a8d2d45ad096
Valeh Ismayilov <101264220+vlhsmylv@users.noreply.github.com>
2023-10-21 16:54:45 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React && TailwindCSS template
+#### Made with ❤️ by [Valeh Ismayilov](https://valehismayilov.vercel.app)
+
+###### How to start to use? 
+
+**Fork** this repo as a template to your account and **clone** it into your device, then **start** to use it!
+
+👾 You can update README.md!
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return(
+        <main className="min-h-screen flex flex-col gap-5 items-center justify-center">
+            <h1 className="text-5xl font-semibold">React & TailwindCSS</h1>
+            <h2>Created with ❤️  by <a href="https://valehismayilov.vercel.app" target="_blank" className="hover:underline font-semibold text-slate-700">Valeh Ismayilov </a></h2> 
+            <a href="https://github.com/vlhsmylv/react-tailwind" target="_blank" className="cursor-pointer hover:underline text-slate-500 text-xl">Github</a> 
+        </main>
+    );
+}
+
+export default App;
